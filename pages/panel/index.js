138cf1aa9676fbd11428411d9605bf5a01f7255e
Sergey Dunaevskiy <sergey@dunaevskiy.eu>
2019-04-11 00:02:02 +0200

    wip: admin change user role form

--- pages/panel/index.js
+++ pages/panel/index.js
@@ -12,7 +12,13 @@ import { actionSetUsageTabBar } from 'actions/tab-bar.action';
 import { actionSetUsageInfoBar } from 'actions/info-bar.action';
 
 import React from 'react';
+import { Field, Form, Formik } from 'formik';
 import CommonLayout from 'layouts/CommonLayout';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSave } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
+const configServer = require('config/server.config');
 
 // -------------------------------------------------------------------------------------------------
 // Component
@@ -33,7 +39,21 @@ class PanelPage extends React.Component {
       // Info Bar
       ctx.store.dispatch(actionSetUsageInfoBar(false));
 
-      return {};
+      // Ajax
+      const token = ctx.store.getState().reducerJWT.token;
+
+      let globalRoles;
+      try {
+         globalRoles = await axios.get(`${configServer.host}/api/dashboard/roles`, {
+            headers: {
+               Authorization: token,
+            },
+         });
+      } catch (err) {}
+
+      return {
+         globalRoles: globalRoles.data.dat.roles,
+      };
    }
 
    // Methods
@@ -45,9 +65,60 @@ class PanelPage extends React.Component {
    render() {
       return (
          <CommonLayout>
-            <div className={'row'}>
-               <div className="col-md-6 col-sm-12">
-                  <h1>Admin panel</h1>
+            <div className="row">
+               <div className="col-6">
+                  <div className="row">
+                     <div className="col">
+                        <h1>Change user role</h1>
+
+                        <Formik
+                           initialValues={{
+                              username: '',
+                              role: 1,
+                           }}
+                           onSubmit={async (values, actions) => {
+                              try {
+                                 await axios.patch(
+                                    `${configServer.host}/api/dashboard/role`,
+                                    values,
+                                 );
+                                 console.log(`Done`);
+                              } catch (e) {
+                                 console.log(`Fail`);
+                              }
+                           }}
+                           render={props => (
+                              <Form>
+                                 <label>
+                                    <span>Username</span>
+                                    <Field type="text" name="username" placeholder="username" />
+                                 </label>
+                                 <label>
+                                    <span>Role</span>
+                                    <Field component="select" name="role">
+                                       {this.props.globalRoles.map(role => {
+                                          return (
+                                             <option key={role.id} value={role.id}>
+                                                {role.name}
+                                             </option>
+                                          );
+                                       })}
+                                    </Field>
+                                 </label>
+                                 <button className="button button_green" type="submit">
+                                    <FontAwesomeIcon icon={faSave} />
+                                    Update user role
+                                 </button>
+                              </Form>
+                           )}
+                        />
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div className="row">
+               <div className="col-12">
+                  <h1>Permissions</h1>
                </div>
             </div>
          </CommonLayout>
